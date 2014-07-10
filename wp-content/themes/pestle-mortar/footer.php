42a4de27cfc6cd6f8297a20895ac36d67cdf0cf0
Joe Fletcher <Joe@merchantguru.com>
2014-07-10 16:10:07 -0700

    Add 2005 to footer

--- wp-content/themes/pestle-mortar/footer.php
+++ wp-content/themes/pestle-mortar/footer.php
@@ -4,7 +4,7 @@
     <?php global $tt_options;
       $tt_settings = get_option( 'tt_options', $tt_options ); 
     ?>
-    <small>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?><?php if( $tt_settings['author_credits'] ) : ?> | Designed by <a href="http://tinktank.in">Tink <span class="tinktank">⚒</span> Tank</a><?php endif; ?></small>
+    <small>&copy; 2005 - <?php echo date("Y"); echo " "; bloginfo('name'); ?><?php if( $tt_settings['author_credits'] ) : ?> | Designed by <a href="http://tinktank.in">Tink <span class="tinktank">⚒</span> Tank</a><?php endif; ?></small>
  
   </footer>
 
